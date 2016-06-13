123a81828af27a6a04019ced2ca2b04aede71e61
Pedro Gómez <pgomez@leadclic.com>
2016-06-13 22:33:51 +0200

    test

--- notebooks/lib.py
+++ notebooks/lib.py
@@ -1,14 +1,14 @@
 def help():
-	print("helloworld() -> Escribe \"hello\"")
-	print("basesDeDatos() -> Devuelve listado de bases de datos disponibles")
-	print("query(db consulta) -> Recibe numero de base de datos y consulta SELECT y escribe resultado de query a la base de datos")
+	print("helloworld()\n   -> Escribe \"helloworld\"\n")
+	print("basesDeDatos()\n   -> Devuelve listado de bases de datos disponibles\n")
+	print("query(db, \"consulta\")\n   -> Recibe numero de base de datos y consulta SELECT y escribe resultado de query a la base de datos\n")
 
 def helloworld():
-	print ("hello")
+	print ("helloworld")
 
 def basesDeDatos():
-	print ("1- Base de datos de Cesce")
-	print ("2- Base de datos de Ejemplo1")
+	print ("1- Base de datos de Cesce\n")
+	print ("2- Base de datos de Ejemplo1\n")
 	print ("3- Base de datos de Ejemplo2")
 	
 def query(db, consulta):
@@ -21,8 +21,6 @@ def query(db, consulta):
 	password = 'p5gvn36g2q076783i6s3j6hqqfq'
 	database = 'dabdmj8rgg0lfa'
 	conn = pg.DB(host=hostname, user=username, passwd=password, dbname=database)
-
 	result = conn.query(consulta)
 	print (result)
-	
 	conn.close()
