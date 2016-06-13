ed8d4d2db834063b902b7da90aec5867efc11717
Pedro Gómez <pgomez@leadclic.com>
2016-06-13 17:10:35 +0200

    test

--- notebooks/lib.py
+++ notebooks/lib.py
@@ -1,2 +1,19 @@
 def helloworld():
-   print ("hello")
+	print ("hello")
+   
+def query(consulta):
+	import pg
+	#Base de datos de Cesce
+	hostname = 'ec2-54-247-190-226.eu-west-1.compute.amazonaws.com'
+	username = 'u3jk39shtaohhp'
+	password = 'p5gvn36g2q076783i6s3j6hqqfq'
+	database = 'dabdmj8rgg0lfa'
+	conn = pg.DB(host="localhost", user="USERNAME", passwd="PASSWORD", dbname="DBNAME")
+
+	result = conn.query(consulta)
+	
+	print ("Nombre de la tabla")
+	for i in result.getresult() :
+		print (i[1])#+"\t****\t"+i[2])
+
+conn.close()
