0065cded3cb157b6a0277a918fc46323c0540344
Pedro Gómez <pgomez@leadclic.com>
2016-06-13 22:14:06 +0200

    test

--- notebooks/lib.py
+++ notebooks/lib.py
@@ -11,11 +11,11 @@ def basesDeDatos():
 	print ("2- Base de datos de Ejemplo1")
 	print ("3- Base de datos de Ejemplo2")
 	
-def query(db consulta):
+def query(db, consulta):
 	import os
 	os.system("pip install pygresql")
 	import pg
-	#Base de datos de Cesce (debe depender del parámetro db que recibe)
+	#Base de datos de Cesce (debe depender del param db que recibe)
 	hostname = 'ec2-54-247-190-226.eu-west-1.compute.amazonaws.com'
 	username = 'u3jk39shtaohhp'
 	password = 'p5gvn36g2q076783i6s3j6hqqfq'
